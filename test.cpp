dc9527b9d6586f76c1e45e2ec46403e88cf08a57
yucathy <asd71312@gmail.com>
2023-01-01 11:23:43 +0800

    delete i and j and add a for loop

--- test.cpp
+++ test.cpp
@@ -9,10 +9,11 @@ int main(){
 
 	std::cout << "i want to fix a bug there." << std:endl;
 
-	int i = 0;
+	for(int i = 0; i < 10; ++i){
 
-	int j = 0;
+		std::cout << i << std::endl;
+	}
 
-	
+
 	return 0;
 }
